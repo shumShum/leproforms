b3600b0d6b763dd2344bd411210ab9974f51bc9f
Gena Shumilkin <GNShumilkin@greenatom.ru>
2021-11-04 13:22:37 +0300

    Chicken power commit.

--- viewout.js
+++ viewout.js
@@ -17,31 +17,32 @@ $(function () {
             var form_hash = this.form_hash;
             var code =
                 "#album\n" +
-                "**" + form_hash['artist'] + " — " + form_hash['album-name'] + " (" + (form_hash['year'] || "2021") + ")**\n\n"
+                "*" + form_hash['artist'] + " – " + form_hash['album-name'] + " (" + (form_hash['year'] || "2021") + ")*\n\n"
 
             var links = "";
-            if (form_hash['vk']) {
-                links += "[Spotify](" + form_hash['spotify'] + ")"
-            }
-            if (form_hash['deezer']) {
-                if (links) { links += " | " }
-                links += "[Deezer](" + form_hash['deezer'] + ")"
-            }
             if (form_hash['spotify']) {
-                if (links) { links += " | " }
-                links += "[Apple](" + form_hash['apple'] + ")"
-            }
-            if (form_hash['yandex']) {
-                if (links) { links += "\n" }
-                links += "[Yandex](" + form_hash['yandex'] + ")"
+                links += "[Spotify](" + form_hash['spotify'] + ")"
             }
             if (form_hash['apple']) {
                 if (links) { links += " | " }
-                links += "[VK](" + form_hash['vk'] + ")"
+                links += "[Apple](" + form_hash['apple'] + ")"
             }
             if (form_hash['youtube']) {
                 if (links) { links += " | " }
                 links += "[YouTube](" + form_hash['youtube'] + ")"
+            }
+            if (form_hash['yandex']) {
+                if (links) { links += " | " }
+                links += "[Yandex](" + form_hash['yandex'] + ")"
+            }
+
+            if (form_hash['vk']) {
+                if (links) { links += "\n" }
+                links += "[VK](" + form_hash['vk'] + ")"
+            }
+            if (form_hash['deezer']) {
+                if (links) { links += " | " }
+                links += "[Deezer](" + form_hash['deezer'] + ")"
             }
 
             code += links + "\n\n" +
@@ -81,11 +82,14 @@ $(function () {
         var album = $('#album-input').val()
         var search = artist + " " + album
 
-        $('#vk-link').attr("href", "https://vk.com/search?c%5Bq%5D=" + search + "&c%5Bsection%5D=auto");
-        $('#deezer-link').attr("href", "https://www.deezer.com/search/" + search)
         $('#spotify-link').attr("href", "https://open.spotify.com/search/" + search)
+        $('#apple-link').attr("href", "https://music.apple.com/us/search?term=" + search)
         $('#youtube-link').attr("href", "https://music.youtube.com/search?q=" + search)
         $('#yandex-link').attr("href", "https://music.yandex.ru/search?text=" + search)
+
+        $('#vk-link').attr("href", "https://vk.com/search?c%5Bq%5D=" + search + "&c%5Bsection%5D=auto");
+        $('#deezer-link').attr("href", "https://www.deezer.com/search/" + search)
+
         $('#lastfm-link').attr("href", "https://www.last.fm/ru/music/" + artist)
         $('#googlepic-link').attr("href", "https://www.google.com/search?tbm=isch&tbs=isz:l&q=" + search)
     }
